4e3b2df1603d0ac003ca120204edaed0f6025128
StrongX <385135499@qq.com>
2016-03-31 23:39:11 +0800

    Initial Commit

---
+++ XGIFView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XGIFView
+//
+//  Created by xlx on 16/3/31.
+//  Copyright © 2016年 xlx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
